2942900b6b39d306088950c936ca3fb651d3d5bf
Chandilene <chandileneborges@gmail.com>
2024-06-12 15:18:55 -0300

    feat: operadores aritmeticos e de atribuição

--- tipos e variaveis/src/edu/chandilene/operadoresemJava/operadores.java
+++ tipos e variaveis/src/edu/chandilene/operadoresemJava/operadores.java
@@ -1,5 +1,27 @@
 package edu.chandilene.operadoresemJava;
 
 public class operadores {
+    public static void main(String[] args) {
+        // String nomeCompleto = "Catia" + "Soares";
+        // System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+        System.out.println(concatenacao);
+
+    }
 
 }
