51bb005902a4136eea4d6f97f66edbaea739880b
Chandilene <chandileneborges@gmail.com>
2024-06-12 15:45:04 -0300

    feat: operadores unarios

---
+++ tipos e variaveis/src/edu/chandilene/operadoresemJava/OperadoresUnarios.java
@@ -0,0 +1,14 @@
+package edu.chandilene.operadoresemJava;
+
+public class OperadoresUnarios {
+    public static void main(String[] args) {
+        int numero = 1; // valores positivos
+        int numeroNegativo = -2; // valor negativo
+
+        System.out.println(++numero); // incrementa
+        System.out.println(--numero); // decrementa
+
+        boolean variavel = true;
+        System.out.println(!variavel);
+    }
+}
