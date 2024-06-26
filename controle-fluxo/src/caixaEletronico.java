f8115c44a86494d6c9f026e477c011e742891a55
Chandilene <chandileneborges@gmail.com>
2024-06-26 16:54:14 -0300

    feat: condicional simples

---
+++ controle-fluxo/src/caixaEletronico.java
@@ -0,0 +1,12 @@
+public class caixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSaque = 27.0;
+
+        if (valorSaque < saldo)
+            saldo = saldo - valorSaque;
+
+        System.out.println(saldo);
+
+    }
+}
