9b0f85e711ea1a75fdecb097bd97464c81453d0e
Chandilene <chandileneborges@gmail.com>
2024-06-26 17:53:01 -0300

    feat: Swith-case

---
+++ controle-fluxo/src/sistemaMedida.java
@@ -0,0 +1,22 @@
+public class sistemaMedida {
+    public static void main(String[] args) {
+        String medida = "P";
+
+        switch (medida) {
+            case "P":
+                System.out.println("Pequeno");
+                break;
+            case "M":
+                System.out.println("Médio");
+                break;
+            case "G":
+                System.out.println("Grande");
+                break;
+
+            default:
+                System.out.println("Indefinido");
+
+        }
+
+    }
+}
