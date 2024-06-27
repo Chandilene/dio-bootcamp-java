559cd741005190e312e01d2637332893701acfaa
Chandilene <chandileneborges@gmail.com>
2024-06-27 14:01:39 -0300

    feat: for com arrays

--- estrutura-de-repeticao/src/forEmArrays.java
+++ estrutura-de-repeticao/src/forEmArrays.java
@@ -1,5 +1,9 @@
 public class forEmArrays {
     public static void main(String[] args) {
+        String alunos[] = { "Pedro", "Lúcia", "Suzanna", "Edmundo" };
 
+        for (int x = 0; x < alunos.length; x++) {
+            System.out.println("O aluno no índice x: " + x + " é o(a) " + alunos[x]);
+        }
     }
 }
